7befcc4d8088bbabbc346c1c67cf62b90654aad0
Andrew Lee <andrewlee@andrews-air-2.home>
2023-09-18 17:41:18 -0400

    Third commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,13 @@ import {
 import axios from "axios";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAGYInm2i6KE9TMMgAkdKrEkf2ieX_ZYKU",
+  authDomain: "crypto-boar.firebaseapp.com",
+  projectId: "crypto-boar",
+  storageBucket: "crypto-boar.appspot.com",
+  messagingSenderId: "972824804417",
+  appId: "1:972824804417:web:0c40acc374f485e23adac0",
+  measurementId: "G-M1XB8K36YH",
 };
 
 const app = initializeApp(firebaseConfig);
